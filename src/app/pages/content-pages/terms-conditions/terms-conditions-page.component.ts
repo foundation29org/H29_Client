10ab7891d3a33fdeb2b06e5c1a5b8ad4525e65bd
f29 javi <javier.logrono@outlook.com>
2022-07-18 12:37:44 +0200

    fix bug popup lang

--- src/app/pages/content-pages/terms-conditions/terms-conditions-page.component.ts
+++ src/app/pages/content-pages/terms-conditions/terms-conditions-page.component.ts
@@ -39,24 +39,27 @@ export class TermsConditionsPageComponent implements OnInit{
 
   ngOnInit() {
     if(this.state=='showPopup'){
-      var htmlcode = '<div class="col-md-12 mt-2 text-left"> <p> ' + this.translate.instant("login.consent") + '</p> <p class="mt-4"> ' + this.translate.instant("login.consent4") + '</p> <p> ' + this.translate.instant("login.consent5") + '</p> <p class="mt-4"> ' + this.translate.instant("login.consent6") + '</p><p> ' + this.translate.instant("login.consent7") + '</p></div>';
-      swal({
-        html: htmlcode,
-        type: undefined,
-        showCancelButton: false,
-        confirmButtonColor: '#0CC27E',
-        cancelButtonColor: '#FF586B',
-        confirmButtonText: this.translate.instant("generics.Close"),
-        showLoaderOnConfirm: true,
-        allowOutsideClick: false
-      }).then((result) => {
-        console.log(result)
-        if (result.value) {
-          //this.sendTerms(true);
-        }else{
-          //this.sendTerms(false);
-        }
-      }).catch(swal.noop);
+      this.translate.use(this.authService.getLang());
+      setTimeout(function () {
+        var htmlcode = '<div class="col-md-12 mt-2 text-left"> <p> ' + this.translate.instant("login.consent") + '</p> <p class="mt-4"> ' + this.translate.instant("login.consent4") + '</p> <p> ' + this.translate.instant("login.consent5") + '</p> <p class="mt-4"> ' + this.translate.instant("login.consent6") + '</p><p> ' + this.translate.instant("login.consent7") + '</p></div>';
+        swal({
+          html: htmlcode,
+          type: undefined,
+          showCancelButton: false,
+          confirmButtonColor: '#0CC27E',
+          cancelButtonColor: '#FF586B',
+          confirmButtonText: this.translate.instant("generics.Close"),
+          showLoaderOnConfirm: true,
+          allowOutsideClick: false
+        }).then((result) => {
+          console.log(result)
+          if (result.value) {
+            //this.sendTerms(true);
+          }else{
+            //this.sendTerms(false);
+          }
+        }).catch(swal.noop);
+      }.bind(this), 500);
     }
   }
 
